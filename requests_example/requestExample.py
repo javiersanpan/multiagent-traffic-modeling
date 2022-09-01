cdefb7b8fcc31dc7ed1c7f14789bc9d3bb43c575
javiersanpan <javiersanpan@outlook.com>
2022-09-01 10:39:43 -0500

    example request script

---
+++ requests_example/requestExample.py
@@ -0,0 +1,16 @@
+import requests
+
+URL = "http://127.0.0.1:5000"
+
+json_example = {
+    "agente": 1,
+    "position": {
+        "x": 10,
+        "y": 20
+    }
+}
+
+# agentpy_json es el endpoint
+r = requests.post(url=URL+"/agentpy_json", json=json_example)
+
+print(r.text)
