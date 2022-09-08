99d7c17871c8451c47def162b501cca291427434
lizgr01 <a01632994@itesm.mx>
2022-09-07 20:17:43 -0500

    Road agents now created over iteration

--- TrafficModeling/multiAgentTraffic.py
+++ TrafficModeling/multiAgentTraffic.py
@@ -13,23 +13,23 @@ class MultiAgentTraffic(ap.Model):
         self.road_background = self.p.road_background
         self.cars_amount = self.p.cars_amount
 
-        #QUITAR
-        self.POSITIONS_X = self.p.POSITIONS_X
-        self.POSITIONS_Y = self.p.POSITIONS_Y
         self.POSITIONS_BG = self.p.POSITIONS_BG
         
         # Create agents
-        self.cars=ap.AgentList(self,2)
-        roadX=ap.AgentList(self,len(self.POSITIONS_X))
-        roadY=ap.AgentList(self,len(self.POSITIONS_Y))
-        roadBG=ap.AgentList(self,len(self.POSITIONS_BG))
+        self.cars=ap.AgentList(self,self.cars_amount)
 
         # Create grid
         self.city = ap.Grid(self, [self.p.size] * 2, track_empty=True)
-        
+
+        road_agents = []
+
+        for i in range(self.cars_amount):
+            road_agents.append(ap.AgentList(self, len(self.roads_positions[i])))
+            self.city.add_agents(road_agents[i], positions=self.roads_positions[i] ,empty=False)
+
+        roadBG=ap.AgentList(self,len(self.POSITIONS_BG))
+
         self.city.add_agents(self.cars, [(26,13),(13,26)])
-        self.city.add_agents(roadX, positions=self.POSITIONS_X ,empty=False)
-        self.city.add_agents(roadY, positions=self.POSITIONS_Y, empty=False)
         self.city.add_agents(roadBG, positions=self.POSITIONS_BG, empty=False)
 
         # Agent type attribute
@@ -40,8 +40,8 @@ class MultiAgentTraffic(ap.Model):
         #2: car 0
         #3: car 1
         
-        roadX.type_agent = 0
-        roadY.type_agent = 0
+        road_agents[0].type_agent = 0
+        road_agents[1].type_agent = 0
         roadBG.type_agent = 0
         
         self.cars[0].type_agent = 2
@@ -50,8 +50,8 @@ class MultiAgentTraffic(ap.Model):
         # Road direction attribute
         #0: is vertical
         #1: is horizontal
-        roadX.road_direction = 0
-        roadY.road_direction = 1
+        road_agents[0].road_direction = 0
+        road_agents[1].road_direction = 1
         roadBG.road_direction = 2
 
     def step(self):
@@ -62,15 +62,11 @@ class MultiAgentTraffic(ap.Model):
                 new_position = self.city.positions[neighbor]
                 
                 if neighbor.type_agent == 0 and neighbor.road_direction == 0 and car.type_agent == 3:
-                    #q carro e
-                    #por q road se va
                     self.city.move_to(car, new_position)
                     neighbor.type_agent = 1
                     break
                     
                 if neighbor.type_agent == 0 and neighbor.road_direction == 1 and car.type_agent == 2:
-                    #q carro es
-                    #por q road se va
                     self.city.move_to(car, new_position)
                     neighbor.type_agent = 1
                     break
