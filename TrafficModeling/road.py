8cf2542599ad0519002e5cf8fee26e2819b60ced
javiersanpan <javiersanpan@outlook.com>
2022-09-09 12:27:45 -0500

    Road class now always draws missing points fixes #21

--- TrafficModeling/road.py
+++ TrafficModeling/road.py
@@ -13,9 +13,9 @@ class Road:
 
         # Set which missing points should be added
         self.draw_north_p = False
-        self.draw_south_p = True
+        self.draw_south_p = False
         self.draw_east_p = False
-        self.draw_west_p = True
+        self.draw_west_p = False
         
     def draw_south(self):
         POSITIONS_DB = []
@@ -63,87 +63,125 @@ class Road:
     def drawRoad(self):
         POSITIONS = []
 
-        # If exit is south
+        # If entry is south
         if self.begin_p[0] == 26 and self.begin_p[1] == 13:
             POSITIONS+=self.draw_south()
             POSITIONS+=self.rab.drawBR()
+            self.draw_south_p = True
             #POSITIONS.append((13,16))
 
             if self.end_p[0] == 13 and self.end_p[1] == 26:
+                # If exit is east
                 POSITIONS+=self.draw_east()
+                self.draw_east_p = True
 
             elif self.end_p[0] == 0 and self.end_p[1] == 13:
+                # If exit is north
                 POSITIONS+=self.draw_north()
                 POSITIONS += self.rab.drawTR()
+                self.draw_north_p = True
+                self.draw_east_p = True
                 #POSITIONS.append((10,13))
             
             elif self.end_p[0] == 13 and self.end_p[1] == 0:
+                # If exit is west
                 POSITIONS+=self.draw_west()
                 POSITIONS += self.rab.drawTR() + self.rab.drawTL()
+                self.draw_west_p = True
+                self.draw_north_p = True
+                self.draw_east_p = True
                 #POSITIONS.append((10,13))
                 #POSITIONS.append((13,10))
             
-        # If exit is east
+        # If entry is east
         elif self.begin_p[0] == 13 and self.begin_p[1] == 26:
             POSITIONS+=self.draw_east()
             POSITIONS+=self.rab.drawTR()
+            self.draw_east_p = True
             #POSITIONS.append((10,13))
 
             if self.end_p[0] == 0 and self.end_p[1] == 13:
+                # If exit is north
+                self.draw_north_p = True
                 POSITIONS+=self.draw_north()
 
             elif self.end_p[0] == 13 and self.end_p[1] == 0:
+                # If exit is west
                 POSITIONS+=self.draw_west()
                 POSITIONS+=self.rab.drawTL()
+                self.draw_north_p = True
+                self.draw_west_p = True
+
                 #POSITIONS.append((13,10))
 
             elif self.end_p[0] == 26 and self.end_p[1] == 13:
+                # If exit is south
                 POSITIONS+=self.draw_south()
                 POSITIONS+=self.rab.drawTL() + self.rab.drawBL()
+                self.draw_north_p = True
+                self.draw_west_p = True
+                self.draw_south_p = True
                 #POSITIONS.append((13,10))
                 #POSITIONS.append((16,13))
 
-        # If exit is north
+        # If entry is north
         elif self.begin_p[0] == 0 and self.begin_p[1] == 13:
             POSITIONS+=self.draw_north()
             POSITIONS+=self.rab.drawTL()
+            self.draw_west_p = True
             #POSITIONS.append((13,10))
+            self.draw_north_p = True
+
 
             if self.end_p[0] == 13 and self.end_p[1] == 0:
+                # If exit is west
                 POSITIONS+=self.draw_west()
             
             elif self.end_p[0] == 26 and self.end_p[1] == 13:
+                # If exit is south
                 POSITIONS+=self.draw_south()
                 POSITIONS+=self.rab.drawBL()
+                self.draw_south_p = True
                 #POSITIONS.append((16,13))
 
             elif self.end_p[0] == 13 and self.end_p[1] == 26:
+                # If exit is east
                 POSITIONS+=self.draw_east()
                 POSITIONS+=self.rab.drawBL() + self.rab.drawBR()
+                self.draw_south_p = True
+                self.draw_east_p = True
                 #POSITIONS.append((16,13))
                 #POSITIONS.append((13,16))
         
-        # If exit is west
+        # If entry is west
         elif self.begin_p[0] == 13 and self.begin_p[1] == 0:
             POSITIONS+=self.draw_west()
             POSITIONS+=self.rab.drawBL()
+            self.draw_west_p = True
+            self.draw_south_p = True
             #POSITIONS.append((16,13))
 
             if self.end_p[0] == 26 and self.end_p[1] == 13:
+                # If exit is south
                 POSITIONS+=self.draw_south()
 
             elif self.end_p[0] == 13 and self.end_p[1] == 26:
+                # If exit is east
                 POSITIONS+=self.draw_east()
                 POSITIONS+=self.rab.drawBR()
                 #POSITIONS.append((13,16))
+                self.draw_east_p = True
+
 
             elif self.end_p[0] == 0 and self.end_p[1] == 13:
+                # If exit is north
                 POSITIONS+=self.draw_north()
                 POSITIONS+=self.rab.drawBR()+self.rab.drawTR()
+                self.draw_east_p = True
+                self.draw_north_p = True
                 #POSITIONS.append((13,16))
                 #POSITIONS.append((0,13))
 
-        
         POSITIONS+=self.draw_missing_points()
 
         return POSITIONS
